815bca3e907aa15e88085266a3a557c4799a83da
Cameron McWatters <cam3rons@yahoo.com>
2017-08-17 17:32:12 -0500

    basic login hookedup

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,22 @@
+module SessionsHelper
+
+  #Logs in the user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  #returns current logged in user
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
+  
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+end
